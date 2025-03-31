806819a8f3f41399ee0c6c7c70253094752a36b1
AndreRimes <andre.2004.rimes@gmail.com>
2025-03-31 11:06:47 -0300

    fix: logout

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,13 +1,24 @@
-import { logoutUser } from "@/utils/auth";
 import { NextResponse } from "next/server";
 
 
 export async function GET() {
     const response = new NextResponse();
     
-    response.cookies.delete('accessToken');
-    response.cookies.delete('refreshToken');
-    logoutUser();
+    response.cookies.delete({
+        name: 'accessToken',
+        path: '/',
+        domain: '.andrerimes.com',
+        secure: true,
+        sameSite: 'strict'
+    });
+    
+    response.cookies.delete({
+        name: 'refreshToken',
+        path: '/',
+        domain: '.andrerimes.com',
+        secure: true,
+        sameSite: 'strict'
+    });
 
     return response;
 }
